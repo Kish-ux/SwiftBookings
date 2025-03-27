636884ab977517532cb3daf980e105d543ec2a04
yvonne <yvonnimooo@gmail.com>
2025-03-27 08:40:45 +0300

    input values

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 document.getElementById("flight-form").addEventListener("submit", function(e) {
     e.preventDefault();
+
+     // Get input values
+     const from = document.getElementById("from").value;
+     const to = document.getElementById("to").value;
+     const departureDate = document.getElementById("departure-date").value;
+     const returnDate = document.getElementById("return-date").value;
+     const passengers = document.getElementById("passengers").value;
+ 
